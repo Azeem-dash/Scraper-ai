b991407c9633c25b0f3225d85cc090af15bd3653
Azeem-dash <azeemshafeeq125@gmail.com>
2025-03-20 15:53:57 +0500

    streaming added

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,8 +1,12 @@
+import 'dart:async';
+
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:markdown/markdown.dart' as md;
 import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
+import 'package:mistralai_client_dart/mistralai_client_dart.dart';
 
+final client = MistralAIClient(apiKey: 'aMMDwxogZkfE5IbI4rRiTNRHrWnnIpDQ');
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -23,6 +27,8 @@ class _HomeScreenState extends State<HomeScreen> {
 
 
   final PageController _pageController = PageController(initialPage: 0);
+  StreamController<String> _streamController = StreamController<String>.broadcast();
+
 
   // Move to the next page
   void nextPage() {
@@ -51,12 +57,19 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   @override
+  void dispose() {
+    _pageController.dispose();
+    _streamController.close();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue.shade700,
         title: Text(
-          'Articles Screen',
+          'Articles',
           style: TextStyle(
             fontSize: 22,
             color: Colors.white,
@@ -80,7 +93,7 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Stack(
         children: [
           _isLoading ? Center(child: CircularProgressIndicator()) : Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 60.0),
+            padding: const EdgeInsets.symmetric(horizontal: 60.0, vertical: 8),
             child: PageView.builder(
               controller: _pageController,
               itemCount: markdownList.length,
@@ -127,8 +140,8 @@ class _HomeScreenState extends State<HomeScreen> {
             child: Align(
               alignment: Alignment.bottomRight,
               child: SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.5,
-                  width: MediaQuery.of(context).size.height * 0.5,
+                  height: MediaQuery.of(context).size.height * 0.6,
+                  width: MediaQuery.of(context).size.height * 0.6,
                   child: _buildChatWindow()
               ),
             ),
@@ -140,78 +153,100 @@ class _HomeScreenState extends State<HomeScreen> {
 
   // Build the chat window (small window on the screen)
   Widget _buildChatWindow() {
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          _isChatWindowOpen = false;
-        });
-      },
-      child: Container(
-        width: double.maxFinite,
-        height: double.maxFinite,
-        margin: EdgeInsets.all(20),
-        padding: EdgeInsets.all(16),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(16),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.1),
-              blurRadius: 8,
-              offset: Offset(0, 4),
-            ),
-          ],
-        ),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Expanded(
-              child: Container(
-                width: double.maxFinite,
-                padding: EdgeInsets.all(8),
-                decoration: BoxDecoration(
-                  color: Colors.blue.shade50,
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: SingleChildScrollView(
-                  child: Text(
-                    _isGenerating ? 'Generating...' : _response.isEmpty ? 'Write Query to get Response...' : _response,
-                    style: TextStyle(fontSize: 16, color: Colors.blue.shade700),
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 10),
+    _queryController.text = '';
 
-            // Query input section
-            TextField(
-              controller: _queryController,
-              textCapitalization: TextCapitalization.sentences,
-              decoration: InputDecoration(
-                labelText: 'Ask your query...',
-                border: OutlineInputBorder(),
+    return Container(
+      width: double.maxFinite,
+      height: double.maxFinite,
+      margin: EdgeInsets.all(20),
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            blurRadius: 8,
+            offset: Offset(0, 4),
+          ),
+        ],
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Expanded(
+            child: Container(
+              width: double.maxFinite,
+              padding: EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: Colors.blue.shade50,
+                borderRadius: BorderRadius.circular(8),
               ),
-              maxLines: 1,
-            ),
-            SizedBox(height: 10),
+              child: SingleChildScrollView(
+                child: StreamBuilder<String>(
+                  stream: _streamController.stream,
+                  builder: (context, snapshot) {
 
-            ElevatedButton(
-              onPressed: () async {
-                if(_isGenerating == false) {
-                  _sendQuery();
-                }
-              },
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue.shade700,
-                  padding: EdgeInsets.symmetric(horizontal: 5, vertical: 4),
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10)))
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      // Show a loading indicator while waiting for data
+                      return Text(
+                        _isGenerating ? 'Generating...' : 'Write query to get response...',
+                        style: TextStyle(fontSize: 16, color: Colors.blue.shade700),
+                      );
+                    } else if (snapshot.hasError) {
+                      // Handle error case
+                      return Text(
+                        'Error: ${snapshot.error}',
+                        style: TextStyle(fontSize: 16, color: Colors.red),
+                      );
+                    } else if (snapshot.hasData) {
+                      // Display the streamed data as it comes in
+                      return Text(
+                        snapshot.data ?? 'No data available',
+                        style: TextStyle(fontSize: 16, color: Colors.blue.shade700),
+                      );
+                    } else {
+                      // Default case when there's no data
+                      return Text(
+                        'No data available',
+                        style: TextStyle(fontSize: 16, color: Colors.blue.shade700),
+                      );
+                    }
+                  },
+                )
               ),
-              child: Text('Ask', style: TextStyle(color: Colors.white),),
             ),
-            SizedBox(height: 10),
-          ],
-        ),
+          ),
+          SizedBox(height: 10),
+
+          // Query input section
+          TextField(
+            controller: _queryController,
+            textCapitalization: TextCapitalization.sentences,
+            decoration: InputDecoration(
+              labelText: 'Ask your query...',
+              border: OutlineInputBorder(),
+            ),
+            maxLines: 1,
+          ),
+          SizedBox(height: 10),
+
+          ElevatedButton(
+            onPressed: () async {
+              if(!_isGenerating) {
+                _sendQuery();
+              }
+            },
+            style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue.shade700,
+                padding: EdgeInsets.symmetric(horizontal: 5, vertical: 4),
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10)))
+            ),
+            child: Text('Ask AI', style: TextStyle(color: Colors.white)),
+          ),
+          SizedBox(height: 10),
+        ],
       ),
     );
   }
@@ -221,39 +256,50 @@ class _HomeScreenState extends State<HomeScreen> {
 
     if (query.isEmpty) return;
 
-    try {
-      setState(() {
-        _isGenerating = true;
-        print('Generating...');
-      });
+    setState(() {
+      _response = '';
+      _isGenerating = true;
+    });
 
-      final response = await dio.post(
-        'http://127.0.0.1:8000/chat', // FastAPI server URL
-        data: {
-          'query': 'Query: $query\n\n\n'
+    try {
+      // Create the ChatCompletionRequest for the stream
+      final request = ChatCompletionRequest(
+        model: 'mistral-small-latest',
+        messages: [
+          UserMessage(content: UserMessageContent.string(
+              'Query: $query\n\n\n'
               'Give answer from this Article ${markdownList[_pageController.page!.toInt()]}\n\n'
               'Note: The data is written in md format you have to give response in simple text only'
-        },  // Sending query in the body
+          )),
+        ],
       );
 
-      // Check if the response is successful
-      if (response.statusCode == 200) {
-        _response = response.data['response'];
-      } else {
-        _response = 'Error: ${response.statusCode}';
+      // Stream data using client.chatStream
+      final stream = client.chatStream(request: request);
+
+      // Iterate over the stream and accumulate the responses
+      await for (final completionChunk in stream) {
+        final chatMessage = completionChunk.choices[0].delta.content;
+        if (chatMessage != null) {
+          // Accumulate the response
+          _response += chatMessage;
+
+          // Add the accumulated response to the stream
+          _streamController.sink.add(_response);
+        }
       }
-      print(_response);
     } catch (e) {
-      print('Failed to get response: $e');
-      _response = 'Failed to get response: $e';
+      setState(() {
+        _response = 'Failed to get response: $e';
+      });
     } finally {
-      _isGenerating = false;
-      setState(() {});
+      setState(() {
+        _isGenerating = false;
+      });
     }
   }
 
   Future<void> _getArticles() async {
-
     try {
       final response = await dio.get(
         'http://127.0.0.1:8000/scraping/fetch', // FastAPI server URL
